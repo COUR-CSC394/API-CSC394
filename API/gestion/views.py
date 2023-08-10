5530602f4e6bda5ecd2ca1af31433f8ed7f6288a
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-08-10 01:17:12 +0000

    Gestion des produita

--- API/gestion/views.py
+++ API/gestion/views.py
@@ -56,3 +56,43 @@ class CategorieDetailAPIView(APIView):
         categorie = self.get_object(pk)
         categorie.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+# Produit Views
+class ProduitListCreateAPIView(APIView):
+    def get(self, request):
+        produits = Produit.objects.all()
+        serializer = ProduitSerializer(produits, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = ProduitSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class ProduitDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Produit.objects.get(pk=pk)
+        except Produit.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        produit = self.get_object(pk)
+        serializer = ProduitSerializer(produit)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        produit = self.get_object(pk)
+        serializer = ProduitSerializer(produit, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        produit = self.get_object(pk)
+        produit.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
