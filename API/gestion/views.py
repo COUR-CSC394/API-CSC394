0421a575e9dcae30c003a3e54aeb8c61ddbfb046
Algorithme123 <kossi.gogo@ipnetinstitute.com>
2023-08-10 01:18:31 +0000

    Gestion des factures

--- API/gestion/views.py
+++ API/gestion/views.py
@@ -135,3 +135,44 @@ class VenteDetailAPIView(APIView):
         vente = self.get_object(pk)
         vente.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+
+# Facture Views
+class FactureListCreateAPIView(APIView):
+    def get(self, request):
+        factures = Facture.objects.all()
+        serializer = FactureSerializer(factures, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = FactureSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class FactureDetailAPIView(APIView):
+    def get_object(self, pk):
+        try:
+            return Facture.objects.get(pk=pk)
+        except Facture.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        facture = self.get_object(pk)
+        serializer = FactureSerializer(facture)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        facture = self.get_object(pk)
+        serializer = FactureSerializer(facture, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        facture = self.get_object(pk)
+        facture.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
